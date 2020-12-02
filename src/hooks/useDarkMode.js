126442c172eb516310580a4184012e066091d04e
Justin Benz <jbenz156@gmail.com>
2020-12-02 16:22:10 -0500

    finished

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,10 @@
+import react from "react"
+
+import useLocalStorage from './useLocalStorage'
+
+const useDarkMode = (key, initialValue) => {
+    const [value, setValue] = useLocalStorage(key, initialValue);
+    return [value, setValue]
+};
+
+export default useDarkMode
